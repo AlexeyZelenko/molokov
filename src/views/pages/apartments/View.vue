d5c67d7f39ab96e7db940d2694b91b54c331438c
Alexey Zelenko <zelenlooleksii.75@gmail.com>
2025-01-25 19:29:10 +0200

    view sell apartment

--- src/views/pages/apartments/View.vue
+++ src/views/pages/apartments/View.vue
@@ -1,5 +1,5 @@
 <template>
-    <h1 class="my-2">Деталі об'єкта нерухомості</h1>
+    <h1 class="font-semibold text-xl mb-2">{{ property.title }}</h1>
 
     <Fluid class="flex flex-col md:flex-row gap-8">
         <div class="md:w-1/2">
@@ -21,32 +21,12 @@
             </div>
 
             <div class="card flex flex-col gap-4">
-                <div class="font-semibold text-xl">Назва</div>
-                <div>{{ property.title }}</div>
-
-                <div class="font-semibold text-xl">Тип нерухомості</div>
-                <div>{{ property.category?.name }}</div>
-
-                <div class="font-semibold text-xl">Мета використання</div>
-                <div>{{ property.subcategory?.name }}</div>
-
-                <div v-if="property.subcategory && property.subcategory.code === 'SALE'" class="font-semibold text-xl">Ціна USD</div>
-                <div v-if="property.subcategory && property.subcategory.code === 'SALE'">{{ property.priceUSD }} $</div>
-
-                <div v-if="property.subcategory && property.subcategory.code !== 'SALE'" class="font-semibold text-xl">Вартість оренди</div>
-                <div v-if="property.subcategory && property.subcategory.code !== 'SALE'">{{ property.priceUSD }} $</div>
-            </div>
-
-            <div class="card flex flex-col gap-4">
                 <div class="font-semibold text-xl">Розташування</div>
-                <div class="font-semibold text-sm">Область</div>
-                <div>{{ property.address.region?.name }}</div>
+                <div class="font-semibold text-sm">Область / Місто</div>
+                <div>{{ property.address.region?.name }} / {{ property.address.city.name }}</div>
 
-                <div class="font-semibold text-sm">Місто</div>
-                <div>{{ property.address.city.name }}</div>
-
-                <div class="font-semibold text-sm">Вулиця</div>
-                <div>{{ property.address.street }}</div>
+                <div class="font-semibold text-sm">Район / Вулиця</div>
+                <div>{{ property.address?.area?.name }} / {{ property.address.street }}</div>
 
                 <GoogleMapAddApartment
                     style="width: 100%; height: 500px"
@@ -55,23 +35,68 @@
                     :disabled="true"
                 />
             </div>
+
+            <div class="card flex flex-col gap-4">
+                <div class="font-semibold text-xl">Стан нерухомості</div>
+                <div>{{ property.condition?.name }}</div>
+
+                <div class="font-semibold text-xl">Тип будівлі</div>
+                <div>{{ property.buildingType?.name }}</div>
+
+                <div class="font-semibold text-xl">Клас об'єкта</div>
+                <div>{{ property.objectClass?.name }}</div>
+
+                <div class="font-semibold text-xl">Ремонт</div>
+                <div>{{ property.reconditioning?.name }}</div>
+            </div>
         </div>
         <div class="md:w-1/2">
+            <div class="card flex flex-col gap-4">
+                <div class="font-semibold text-xl">
+                    {{ property.category?.name }} / {{ property.subcategory?.name }}
+                </div>
+
+                <div v-if="property.subcategory && property.subcategory.code === 'SALE'">{{ property.priceUSD }} $</div>
+
+                <div v-if="property.subcategory && property.subcategory.code !== 'SALE'">{{ property.priceUSD }} $</div>
+            </div>
+
             <div class="card flex flex-col gap-4">
                 <div class="font-semibold text-xl">Площа(м²)</div>
                 <div class="font-semibold text-sm">Загальна площа</div>
                 <div>{{ property.apartmentArea.totalArea }} м²</div>
 
                 <div class="font-semibold text-sm">Жила площа квартири</div>
-                <div>{{ property.apartmentArea.livingArea }} м²</div>
+                <div>{{ property.apartmentArea.livingArea }} м² / {{property.planning?.name}}</div>
 
                 <div class="font-semibold text-sm">Площа кухні</div>
                 <div>{{ property.apartmentArea.kitchenArea }} м²</div>
+
+                <div class="font-semibold text-sm">Санвузел - {{ property.bathroom?.name }}</div>
             </div>
 
             <div class="card flex flex-col gap-4">
                 <div class="font-semibold text-xl">Тип опалення</div>
                 <div>{{ property.heatingType?.name }}</div>
+            </div>
+
+            <div class="card flex flex-col gap-4">
+                <div class="font-semibold text-xl">Комунікації</div>
+                <div>{{ property.communications }}</div>
+
+                <div class="font-semibold text-xl">Інфраструктура</div>
+                <div>{{ property.infrastructure }}</div>
+
+                <div class="font-semibold text-xl">Ландшафт</div>
+                <div>{{ property.landscape }}</div>
+
+                <div class="font-semibold text-xl">Проживання тварин</div>
+                <div>{{ property.animal ? 'Так' : 'Ні' }}</div>
+            </div>
+
+            <div v-if="property?.facilityReadiness" class="card flex flex-col gap-4">
+                <div class="font-semibold text-xl">Готовність об'єкта</div>
+                <div>{{ formatFirebaseTimestamp(property?.facilityReadiness) }}</div>
             </div>
 
             <div class="card flex flex-col gap-4">
@@ -92,12 +117,6 @@
                 <div class="font-semibold text-xl">Паркування</div>
                 <div>{{ property.parking?.name }}</div>
             </div>
-
-            <div class="card flex flex-col gap-4">
-                <div class="font-semibold text-xl">Контакти</div>
-                <div>{{ property.owner?.username }}</div>
-                <div v-if="property.owner?.message" class="mt-2">{{ property.owner?.message }}</div>
-            </div>
         </div>
     </Fluid>
 
@@ -106,6 +125,33 @@
             <div class="font-semibold text-xl">Додадковий опис об'єкта</div>
             <div>{{ property.description }}</div>
         </div>
+    </Fluid>
+
+    <Fluid class="grid grid-cols-1 md:grid-cols-2 gap-6 mt-8">
+        <!-- Блок "контакти ріелтора" -->
+        <div class="card flex flex-col gap-6 p-4 shadow-lg rounded-lg bg-white">
+            <div class="card flex flex-col gap-2">
+                <div class="font-semibold text-xl">Контакти ріелтора</div>
+                <div>{{ property.creator?.username }}</div>
+                <div>Телефон: {{ property.creator?.phone }}</div>
+                <div v-if="property.creator?.message">
+                    {{ property.creator?.message }}
+                </div>
+            </div>
+        </div>
+        <!-- Блок "Інформація про власника" -->
+        <div class="card flex flex-col gap-6 p-4 shadow-lg rounded-lg bg-white">
+            <div class="card flex flex-col gap-2">
+                <div class="font-semibold text-xl">Контакти власника / посередника</div>
+                <div>{{ property.owner?.username }} - {{ property.typeOwner?.name }}</div>
+                <div>Телефон: {{ property.owner?.phone }}</div>
+                <div v-if="property.owner?.message">
+                    {{ property.owner?.message }}
+                </div>
+            </div>
+        </div>
+
+
     </Fluid>
 
     <Fluid class="flex mt-8">
@@ -125,6 +171,7 @@ import { doc, getDoc } from 'firebase/firestore';
 import { useToast } from 'primevue/usetoast';
 import Toast from 'primevue/toast';
 import GoogleMapAddApartment from '@/components/googleMap/AddApartment.vue';
+import { formatFirebaseTimestamp } from '@/utils/dateUtils';
 
 const toast = useToast();
 const router = useRouter();
@@ -151,7 +198,12 @@ const property = ref({
     utilities: [],
     furniture: null,
     parking: null,
-    description: ''
+    description: '',
+    creator: {
+        username: '',
+        phone: '',
+        message: ''
+    },
 });
 
 onMounted(async () => {
