c14d6ef8c1a6e948f2eed9916ea0c60d040ebf47
Alexey Zelenko <zelenlooleksii.75@gmail.com>
2025-02-20 16:48:06 +0200

    change index files

--- src/components/properties/view/areas/HouseArea.vue
+++ src/components/properties/view/areas/HouseArea.vue
@@ -1,14 +1,16 @@
 <template>
     <div class="card flex flex-col gap-4">
         <div class="font-semibold text-xl">Площа(м²)</div>
-        <div class="font-semibold text-sm">Загальна площа будинку</div>
+        <div class="font-semibold text-sm">Загальна площа</div>
         <div>{{ area.totalArea }} м²</div>
 
-        <div class="font-semibold text-sm">Площа ділянки</div>
-        <div>{{ area.landArea }} м²</div>
+        <div class="font-semibold text-sm">Жила площа квартири</div>
+        <div>{{ area.livingArea }} м² / {{planning?.name}}</div>
 
-        <div class="font-semibold text-sm">Кількість поверхів</div>
-        <div>{{ area.floors }}</div>
+        <div class="font-semibold text-sm">Площа кухні</div>
+        <div>{{ area.kitchenArea }} м²</div>
+
+        <div class="font-semibold text-sm">Санвузел - {{ bathroom?.name }}</div>
     </div>
 </template>
 
@@ -16,7 +18,18 @@
 defineProps({
     area: {
         type: Object,
-        required: true
+        required: true,
+        validator: (value) => {
+            return value.totalArea && value.livingArea && value.kitchenArea;
+        }
+    },
+    planning: {
+        type: Object,
+        default: null
+    },
+    bathroom: {
+        type: Object,
+        default: null
     }
 });
 </script>
--- src/components/properties/view/categories/house/index.vue
+++ src/components/properties/view/categories/house/index.vue
@@ -1,11 +1,4 @@
 <template>
-    <div class="card flex flex-col gap-4">
-        <div class="font-semibold text-xl">
-            {{ property.category?.name }} / {{ property.subcategory?.name }}
-        </div>
-        <PriceConverter :price="property.price" isDisplayUAH class="font-bold text-xl"/>
-    </div>
-
     <AreaDetails
         :apartmentArea="property.apartmentArea"
         :planning="property.planning"
@@ -18,26 +11,18 @@
         :objectClass="property.objectClass"
         :reconditioning="property.reconditioning"
     />
-    <component
-        v-if="property"
-        :is="subcategoryComponentMap[property?.subcategory?.code] || SellApartment"
-        :property="property"
-    />
-
-    <ParkingDetails :parking="property.parking" />
 </template>
 
 <script setup>
 import { defineProps } from 'vue';
+import PriceConverter from "@/components/price/PriceConverter.vue";
 import AreaDetails from "./AreaDetails.vue";
 import PropertyDetails from "./PropertyDetails.vue";
-import ParkingDetails from "./ParkingDetails.vue";
 
 import SellApartment from './sell/index.vue';
 import RentApartment from './rent/index.vue';
 import ExchangeApartment from './exchange/index.vue';
 import DailyRentApartment from './daily/index.vue';
-import PriceConverter from "@/components/price/PriceConverter.vue";
 
 
 const props = defineProps({
