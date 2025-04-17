316b90136b8fe953996fa51ab0613c3f80770c8b
Alexey Zelenko <zelenlooleksii.75@gmail.com>
2025-04-18 01:55:41 +0300

    select

--- src/components/forms/BlockSearchHomePage.vue
+++ src/components/forms/BlockSearchHomePage.vue
@@ -42,21 +42,28 @@ const selectedRegion = computed({
 
 // Bedrooms
 const bedroomOptions = [
-    { label: 'Всі', value: '' },
-    { label: '1', value: 1 },
-    { label: '2', value: 2 },
-    { label: '3', value: 3 },
-    { label: '4', value: 4 },
-    { label: '5', value: 5 },
-    { label: '6', value: 6 },
-    { label: '7', value: 7 },
-    { label: '8', value: 8 },
-    { label: '9', value: 9 },
-    { label: '10', value: 10 }    
+    { label: 'Всі', value: [] },
+    { label: '1', value: [1] },
+    { label: '2', value: [2] },
+    { label: '3', value: [3] },
+    { label: '4', value: [4] },
+    { label: '5', value: [5] },
+    { label: '6', value: [6] },
+    { label: '7', value: [7] },
+    { label: '8', value: [8] },
+    { label: '9', value: [9] },
+    { label: '10', value: [10] },
+    { label: '1-2', value: [1, 2] },
+    { label: '2-3', value: [2, 3] },
+    { label: '3-4', value: [3, 4] },
+    { label: '4+', value: [4, 5, 6, 7, 8, 9, 10] }
 ];
 const selectedRooms = computed({
-  get: () => areasStore.getFilters['rooms.all'] || '',
-  set: val => areasStore.setFilter('rooms.all', Number(val))
+    get: () => {
+        const rooms = areasStore.getFilters['rooms.all'];
+        return Array.isArray(rooms) ? rooms : [];
+    },
+    set: val => areasStore.setFilter('rooms.all', val)
 });
 
 // Budget options
@@ -116,7 +123,7 @@ const resetFilters = () => {
     selectedTypes.value = null;
     activeStatus.value = '';
     selectedRegion.value = '';
-    selectedRooms.value = '';
+    selectedRooms.value = [];
     selectedBudget.value = '';
     showError.value = false;
     errorMessage.value = '';
